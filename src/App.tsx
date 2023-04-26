8d6226e7252f0bcc7b8cd2045c314b7fc7f0bccd
ptrst102 <ptrst102@gmail.com>
2023-04-26 23:05:37 +0900

    リセットボタン

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import { css } from '@emotion/react';
 import { GlobalStyles } from './styles/global';
+import { useState } from 'react';
 
 export const App = () => {
+  const [lifeA, setLifeA] = useState(8000);
+  const [lifeB, setLifeB] = useState(8000);
+
+  const handleReset = () => {
+    setLifeA(8000);
+    setLifeB(8000);
+  };
+
   return (
     <>
       <GlobalStyles />
@@ -9,9 +18,38 @@ export const App = () => {
         css={css`
           width: 100vw;
           height: 100vh;
+          display: flex;
         `}
       >
-        hello world
+        <div
+          css={css`
+            flex: 1;
+          `}
+        >
+          {lifeA}
+        </div>
+        <div
+          css={css`
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          `}
+        >
+          <button
+            onClick={() => {
+              handleReset();
+            }}
+          >
+            reset
+          </button>
+        </div>
+        <div
+          css={css`
+            flex: 1;
+          `}
+        >
+          {lifeB}
+        </div>
       </div>
     </>
   );
